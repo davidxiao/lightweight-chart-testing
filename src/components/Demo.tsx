9768da568c0638aa36502fa0460cfc6662938247
David Xiao <4176280+davidxiao@users.noreply.github.com>
2022-07-11 17:23:40 +1000

    testing

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -5,12 +5,12 @@ import { createChart, IChartApi } from "lightweight-charts";
 import { Props } from "./model";
 
 import { Legend } from "./Legend";
-import { ToolTip } from "./ToolTip";
 
 export const Demo = (props: Props) => {
   const { chartOptions, panes } = props;
   const containerRef = useRef<HTMLDivElement>(null);
   const [chart, setChart] = useState<IChartApi>();
+  const [seriesReady, setSeriesReady] = useState(false);
   const [paneRefs, setPaneRefs] = useState<HTMLElement[]>();
   useEffect(() => {
     if (!containerRef || !containerRef.current) {
@@ -77,11 +77,71 @@ export const Demo = (props: Props) => {
         }
       });
     });
-
-    if (!chart) return;
-    setPaneRefs(chart.getPaneElements());
+    setSeriesReady(true);
   }, [chart, panes]);
+  useEffect(() => {
+    if (!chart) return;
+    console.log(`chart.getPaneElements:=${chart.getPaneElements().length}`);
+    setPaneRefs(chart.getPaneElements());
+  }, [chart, seriesReady]);
 
+  /*
+  useEffect(() => {
+    var container = document.createElement("div");
+    document.body.appendChild(container);
+    var toolTipWidth = 80;
+    var toolTipHeight = 80;
+    var toolTipMargin = 15;
+
+    var toolTip = document.createElement("div");
+    toolTip.className = "floating-tooltip-2";
+    container.appendChild(toolTip);
+
+    chart?.subscribeCrosshairMove(function (param) {
+      if (
+        param.point === undefined ||
+        !param.time ||
+        param.point.x < 0 ||
+        param.point.x > container.clientWidth ||
+        param.point.y < 0 ||
+        param.point.y > container.clientHeight
+      ) {
+        toolTip.style.display = "none";
+      } else {
+        const dateStr = new Date().toString();
+        toolTip.style.display = "block";
+        var price = 1200;
+        toolTip.innerHTML =
+          '<div style="color: #009688">Apple Inc.</div><div style="font-size: 24px; margin: 4px 0px; color: #21384d">' +
+          Math.round(100 * price) / 100 +
+          '</div><div style="color: #21384d">' +
+          dateStr +
+          "</div>";
+        var coordinate = series.priceToCoordinate(price);
+        var shiftedCoordinate = param.point.x - 50;
+        if (coordinate === null) {
+          return;
+        }
+        shiftedCoordinate = Math.max(
+          0,
+          Math.min(container.clientWidth - toolTipWidth, shiftedCoordinate)
+        );
+        var coordinateY =
+          coordinate - toolTipHeight - toolTipMargin > 0
+            ? coordinate - toolTipHeight - toolTipMargin
+            : Math.max(
+                0,
+                Math.min(
+                  container.clientHeight - toolTipHeight - toolTipMargin,
+                  coordinate + toolTipMargin
+                )
+              );
+        toolTip.style.left = shiftedCoordinate + "px";
+        toolTip.style.top = coordinateY + "px";
+      }
+    });
+  }, [chart]);
+  */
   return (
     <div
       ref={containerRef}
@@ -98,16 +158,6 @@ export const Demo = (props: Props) => {
             meta={panes[idx].meta}
             series={panes[idx].series}
           ></Legend>
-        );
-      })}
-      {paneRefs?.map((ref, idx) => {
-        return (
-          <ToolTip
-            key={`tooltip-${panes[idx].meta.title}-${idx}`}
-            paneRef={ref}
-            meta={panes[idx].meta}
-            series={panes[idx].series}
-          ></ToolTip>
         );
       })}
     </div>
